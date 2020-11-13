cdf038a2bbf3ad400c76e4e0e9f3e7080f9f54b2
jerangel <jerangel@indracompany.com>
2020-11-13 10:30:20 -0500

    Ciclo de vida de React

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
+      usuarios: [],
+    };
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    console.log(respuesta);
+    this.setState({
       usuarios: [
         {
           nombre: "Jorge",
@@ -17,7 +28,7 @@ class App extends Component {
           enlace: "pepe.com",
         },
       ],
-    };
+    });
   }
 
   ponerFilas = () =>
@@ -30,6 +41,7 @@ class App extends Component {
     ));
 
   render() {
+    console.log(this.state.usuarios);
     return (
       <div className="margen">
         <table className="tabla">
