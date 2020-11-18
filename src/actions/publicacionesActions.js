1aa9e5ecd204276893fb64e0d73ce178917e0516
jerangel <jerangel@indracompany.com>
2020-11-18 08:30:56 -0500

    Llamando a múltiples reducers en una acción

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,12 +1,24 @@
 import axios from "axios";
+import { CARGANDO, ERROR, TRAER_TODOS } from "../types/publicacionesTypes";
 
 export const traerTodos = () => async (dispatch) => {
-  const respuesta = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-
   dispatch({
-    type: "traer_todos",
-    payload: respuesta.data,
+    type: CARGANDO,
   });
+
+  try {
+    const respuesta = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    dispatch({
+      type: TRAER_TODOS,
+      payload: respuesta.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: "Algo salió mal, intente mas tarde.",
+    });
+  }
 };
